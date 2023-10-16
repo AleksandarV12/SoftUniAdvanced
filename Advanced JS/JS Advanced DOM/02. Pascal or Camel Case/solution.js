3025a9e3a684938bea50e7b627eb3bf00a6b36b4
AleksandarV12 <aleksandyr.velkov12@gmail.com>
2023-10-17 00:26:26 +0200

    Did some exercises

--- Advanced JS/JS Advanced DOM/02. Pascal or Camel Case/solution.js
+++ Advanced JS/JS Advanced DOM/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById("text").value;
+  let convention = document.getElementById("naming-convention").value;
+  let res = "";
+  switch (convention) {
+    case "Camel Case":
+      let textArr = text.toLocaleLowerCase().split(" ");
+      res += textArr.shift();
+      textArr.forEach((e) => {
+        res += e[0].toUpperCase() + e.substring(1);
+      });
+      break;
+    case "Pascal Case":
+      text
+        .toLowerCase()
+        .split(" ")
+        .forEach((e) => (res += e[0].toUpperCase() + e.substring(1)));
+      break;
+    default:
+      res = "Error!";
+      break;
+  }
+  document.getElementById("result").textContent = res;
 }
