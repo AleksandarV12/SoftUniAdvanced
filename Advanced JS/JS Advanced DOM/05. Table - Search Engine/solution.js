3474084803011033f3ccb23ca371805c0d164fe5
AleksandarV12 <aleksandyr.velkov12@gmail.com>
2023-10-19 02:28:16 +0200

    Some Dom exercises

--- Advanced JS/JS Advanced DOM/05. Table - Search Engine/solution.js
+++ Advanced JS/JS Advanced DOM/05. Table - Search Engine/solution.js
@@ -1,8 +1,20 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-
-   function onClick() {
-      //   TODO:
-
-   }
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  let searchField = document.getElementById("searchField");
+  let tableRow = Array.from(document.querySelectorAll("tbody tr"));
+  function onClick() {
+    let searchText = searchField.value;
+    for (let row of tableRow) {
+      let tableData = row.querySelectorAll("td");
+      for (let td of tableData) {
+        if (td.textContent.includes(searchText)) {
+          row.classList.add("select");
+          break;
+        } else {
+          row.classList.remove("select");
+        }
+      }
+    }
+    searchField.value = "";
+  }
 }
