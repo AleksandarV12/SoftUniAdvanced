768b77db2c4cc88f2e308244e2ff425d0cc13298
AleksandarV12 <aleksandyr.velkov12@gmail.com>
2023-10-19 19:35:01 +0200

    Just finished some DOM exercises

--- Advanced JS/JS Advanced DOM/06. Format the Text/solution.js
+++ Advanced JS/JS Advanced DOM/06. Format the Text/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+  const inputText = document.getElementById("input").value;
+
+  const sentences = inputText
+    .split(".")
+    .filter((sentence) => sentence.trim() !== "");
+
+  const paragraphs = [];
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    const paragraphSentences = sentences.slice(i, i + 3);
+
+    const paragraphText = paragraphSentences.join(". ") + ".";
+
+    const paragraphElement = `<p>${paragraphText}</p>`;
+
+    paragraphs.push(paragraphElement);
+  }
+
+  const outputDiv = document.getElementById("output");
+
+  outputDiv.innerHTML = "";
+
+  paragraphs.forEach((paragraph) => {
+    outputDiv.innerHTML += paragraph;
+  });
 }
--- Advanced JS/JS Advanced DOM/08. Generate Report/report.js
+++ Advanced JS/JS Advanced DOM/08. Generate Report/report.js
@@ -1,3 +1,28 @@
 function generateReport() {
-    //TODO
+  const table = document.querySelector("table");
+  const headers = table.querySelectorAll("thead input[type=checkbox]");
+  const rows = table.querySelectorAll("tbody tr");
+
+  const selectedColumns = Array.from(headers)
+    .map((checkbox, index) => ({
+      index,
+      name: checkbox.name,
+      checked: checkbox.checked,
+    }))
+    .filter((column) => column.checked);
+
+  const reportData = Array.from(rows).map((row) => {
+    const rowData = selectedColumns.reduce((result, column) => {
+      const cellText = row.querySelector(
+        `td:nth-child(${column.index + 1})`
+      ).textContent;
+      result[column.name] = cellText;
+      return result;
+    }, {});
+
+    return rowData;
+  });
+
+  const outputTextarea = document.getElementById("output");
+  outputTextarea.value = JSON.stringify(reportData, null, 2);
 }
