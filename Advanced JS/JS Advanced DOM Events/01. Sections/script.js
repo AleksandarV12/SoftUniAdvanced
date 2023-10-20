c015a8557f00fbd33e45bdaa5dbf42d3a9ad23c1
AleksandarV12 <aleksandyr.velkov12@gmail.com>
2023-10-20 19:54:45 +0200

    Some advanced DOM exercises

---
+++ Advanced JS/JS Advanced DOM Events/01. Sections/script.js
@@ -0,0 +1,17 @@
+function create(words) {
+  const contentRef = document.getElementById("content");
+  for (let word of words) {
+    let divRef = document.createElement("div");
+    let pRef = document.createElement("p");
+    pRef.style.display = "none";
+    pRef.textContent = word;
+    divRef.appendChild(pRef);
+    contentRef.appendChild(divRef);
+    divRef.addEventListener("click", onClickHandler);
+  }
+  function onClickHandler(e) {
+    let divRef = e.currentTarget;
+    let pRef = divRef.children[0];
+    pRef.style.display = "block";
+  }
+}
